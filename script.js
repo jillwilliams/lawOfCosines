7c2dd7baaf2c923cd725833b25f22a99bafa080d
jillwilliams <codeGeek.03.14@gmail.com>
2020-08-28 16:19:45 -0700

    Final Draft

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ const lawOfCosines = ( ) => {
     if (a != 0 && b != 0 && c == 0) {
         var calculate = (a*a + b*b - 2*a*b*Math.cos(angle));
         var calculate2 =  Math.sqrt(calculate);
-            return "Side c = " + calculate2.toFixed(2) + " un";
+            return "Side c = " + calculate2.toFixed(1) + " un";
     } else
     if (a != 0 && c != 0 && b == 0) {
         var calc1 = (a*a + c*c - 2*a*c*Math.cos(angle));
         var calc2 = Math.sqrt(calc1);
-            return "Side b = " + calc2.toFixed(2) + " un";
+            return "Side b = " + calc2.toFixed(1) + " un";
     } else
     if (b != 0 && c != 0 && a == 0) {
         var calc3 = (b*b + c*c - 2*b*c*Math.cos(angle));
         var calc4 = Math.sqrt(calc3);
-            return "Side a = " + calc4.toFixed(2) + " un";
+            return "Side a = " + calc4.toFixed(1) + " un";
     } else
         return "Re-enter your Values";
 }
